62f32af6d3f8dd09956d0595e928e7f3d805ba2b
Mel <angel.melendez.c@gmail.com>
2023-11-27 12:13:08 -0600

    set Chakra-UI, create AppBar nav, add basic pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { type AppType } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import theme from "~/styles/theme";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
